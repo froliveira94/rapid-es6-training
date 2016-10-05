e2cfe40fd3b93ad2c5af4a8222bf85ae87fce01f
Felipe Rodrigues <fr.oliveira94@gmail.com>
2016-10-05 11:10:22 -0300

    octal-and-binary-literals

---
+++ new-es6-syntax/octal-and-binary-litrals/es6.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var value = 0o10;
+console.log(value); //8
+
+var value_a = 0O10;
+console.log(value_a); //8
+
+var value_b = 0b10;
+console.log(value_b); //2
+
+var value_c = 0B10;
+console.log(value_c); //2
