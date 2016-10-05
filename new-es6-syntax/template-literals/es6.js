92880c644784cb8d8b2f35394b2441f3ff416477
Felipe Rodrigues <fr.oliveira94@gmail.com>
2016-10-05 11:22:40 -0300

    template-literals - somme errors

---
+++ new-es6-syntax/template-literals/es6.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let invoiceNum = '1350'; 
+console.log('Invoice Number: ${invoiceNum}');
+console.log('Invoce Number: ${"INV-" + invoiceNum}');
+
+/*let message = 'A
+B 
+C';
+console.log(message);*/
